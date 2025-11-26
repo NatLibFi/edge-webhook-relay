b95f56cb9387be9ad2c820d4dcc28c9e724ece62
aatuny <27770287+aatuny@users.noreply.github.com>
2025-11-26 12:15:04 +0200

    Fix process naming in shutdown log

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ export default async function ({
 
   // Soft shutdown function
   server.on('close', () => {
-    logger.info('Initiating soft shutdown of Melinda REST API');
+    logger.info('Initiating soft shutdown of edge-webhook-relay');
     // Things that need soft shutdown
   });
 
